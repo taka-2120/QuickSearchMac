5a3717ce163e3c049cef3cfcacbfd104f4f5100a
taka-2120 <yu_tkhs@icloud.com>
2024-12-18 23:59:54 +0900

    chore: add auto dmg creation

--- Casks/quick-search.rb
+++ Casks/quick-search.rb
@@ -2,7 +2,7 @@ cask "quick-search" do
   version "1.0.0"  
   sha256 "a785cb75827f0c6e1c71b9a7ecf022894360d2fc6476b2938612cd387121a1f5" 
 
-  url "https://your-hosting-url.com/quick-search-#{version}.dmg"
+  url "https://github.com/taka-2120/QuickSearchMac/releases/download/#{version}/QuickSearch.dmg"
   name "QuickSearch"
   desc "Search on Web Quickly"
   homepage "https://github.com/taka-2120/QuickSerachMac" 
